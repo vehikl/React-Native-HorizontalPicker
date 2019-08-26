5766b01448456111bcadc78ca0b0af336b762486
Vehikl Team <go@vehikl.com>
2019-08-26 10:33:13 -0400

    Initial Commit

---
+++ horizontal-picker/index.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-native-testing-library';
+import HorizontalPicker from './index';
+
+describe('horizontal-picker', () => {
+  it('can render', () => {
+    const component = render(<HorizontalPicker />);
+  });
+});
