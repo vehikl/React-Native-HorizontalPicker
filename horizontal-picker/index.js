4370fcf6ab9299486c95401e5cdcffc41dc7fb03
Vehikl Team <go@vehikl.com>
2019-09-05 11:00:19 -0400

    Stylin'

--- horizontal-picker/index.js
+++ horizontal-picker/index.js
@@ -13,32 +13,74 @@ export default function HorizontalPicker(props) {
   }
 
   function renderItem({ item }) {
+    let tacos;
+    if (item === selectedNumber) {
+      tacos = (<Text>^</Text>);
+    }
+
     return (
       <TouchableHighlight onPress={() => updateSelectedNumber(item)}>
-        <Text style={{ padding: 3 }}>{item}</Text>
+        <View style={styles.numberDisplay}>
+          {tacos}
+          <Text style={styles.numberText}>{item}</Text>
+          <Text>|</Text>
+        </View>
       </TouchableHighlight>
     );
   }
 
+  function getInitialScrollIndex() {
+    return range.findIndex((item) => item === props.initialValue - 3);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.selectedItem} testID="number">{selectedNumber}</Text>
+      <View style={styles.selectionContainer}>
+        <Text style={styles.selectedItem} testID="number">{selectedNumber}</Text>
+      </View>
       <FlatList
         horizontal
         keyExtractor={(item, index) => `${item}-${index}`}
         data={range}
         renderItem={renderItem}
+        getItemLayout={(data, index) => (
+          { length: 60, offset: 60 * index, index }
+        )}
+        initialScrollIndex={getInitialScrollIndex()}
       />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  numberDisplay: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 60
+  },
+  numberText: {
+    fontSize: 24,
+    color: 'rgba(255, 255, 255, 0.8)',
+  },
   container: {
     width: '100%',
+    backgroundColor: '#ff9966',
+    borderRadius: 50,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    paddingVertical: 80,
+  },
+  selectionContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   selectedItem: {
-
+    padding: 100,
+    fontSize: 60,
+    color: 'white',
+    fontWeight: '800',
   },
 });
 
