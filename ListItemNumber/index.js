5ae812692a664f1b3ef64a1eb0d2dcb8cb17e175
Vehikl Team <go@vehikl.com>
2019-09-18 10:06:54 -0400

    Popped that chevron off

--- ListItemNumber/index.js
+++ ListItemNumber/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import { TouchableHighlight, View, Text, StyleSheet } from 'react-native';
 
-
 function ListItemNumber(props) {
   const {item, selectedNumber, updateSelectedNumber} = props
-  let indicatorArrow;
-  if (item === selectedNumber) {
-    indicatorArrow = (<Text>^</Text>);
-  }
 
   return (
     <TouchableHighlight onPress={() => updateSelectedNumber({ item })}>
       <View style={styles.numberDisplay}>
-        {indicatorArrow}
         <Text style={styles.numberText}>{item}</Text>
         <Text style={styles.numberText}>|</Text>
       </View>
