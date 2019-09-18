9d29d55fcef94c0ec186ca315827afe34804264d
Justin Buchanan <jstnbchnn@gmail.com>
2019-09-18 15:03:57 -0400

    Sets up eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": [
+        "airbnb",
+        "plugin:react/recommended"
+    ],
+    'parser': 'babel-eslint',
+    'env': {
+        'jest': true
+    },
+    'plugins': [
+        "react"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    }
 };
